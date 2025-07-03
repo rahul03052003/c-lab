6b81109fdca5701911aa4510de50c8a07a167a86
rahul03052003 <143925767+rahul03052003@users.noreply.github.com>
2025-07-04 04:46:24 +0530

    Update postfixevaluation.c

--- postfixevaluation.c
+++ postfixevaluation.c
@@ -1,53 +1,54 @@
+#include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <math.h>
 
-#define SIZE 30
+#define STACK_SIZE 30
 
-double compute(char symbol, double op1, double op2) {
-    switch (symbol) {
-        case '+': return op1 + op2;
-        case '-': return op1 - op2;
-        case '*': return op1 * op2;
-        case '/': return op1 / op2;
-        case '^':
-        case '$': return pow(op1, op2); // Treating '$' as exponentiation
-        default: printf("Invalid operator: %c\n", symbol); exit(1);
-    }
+double compute(char symbol, double op1, double op2)
+{
+switch(symbol){
+case '+':return op1+op2;
+case '-':return op1-op2;
+case '*':return op1*op2;
+case '/':return op1/op2;
+case '^':
+case '$':return pow(op1,op2);
+default:printf("Invalid operator: %c\n",symbol);exit(1);
+}
 }
 
-int main() {
-    double s[SIZE], res, op1, op2;
-    int top = -1, i;
-    char postfix[SIZE], sym;
+void main()
+{
+double s[STACK_SIZE],res,op1,op2;
+int top=-1,i;
+char postfix[STACK_SIZE],sym;
 
-    printf("Enter a valid postfix expression:\n");
-    scanf("%s", postfix);
+printf("Enter a valid postfix expression:");
+scanf("%s",postfix);
 
-    for (i = 0; i < strlen(postfix); i++) {
-        sym = postfix[i];
+for(i=0;i<strlen(postfix);i++){
+sym=postfix[i];
 
-        if (isdigit(sym)) {
-            s[++top] = sym - '0';
-        } else {
-            if (top < 1) {  // Validate correct operand count
-                printf("Invalid postfix expression\n");
-                return 1;
-            }
-            op2 = s[top--];
-            op1 = s[top--];
-            res = compute(sym, op1, op2);
-            s[++top] = res;
-        }
-    }
-
-    if (top != 0) {  // Ensure stack contains only one result
-        printf("Error: Invalid postfix expression\n");
-        return 1;
-    }
-
-    printf("The result = %f\n", s[top]);
-    return 0;
+if(isdigit(sym))
+s[++top]=sym-'0';
+else{
+if(top<1){
+printf("Invalid postfix expression\n");
+exit(1);
+}
+op2=s[top--];
+op1=s[top--];
+res=compute(sym,op1,op2);
+s[++top]=res;
+}
 }
 
+if(top!=0){
+printf("Error: Invalid postfix expression\n");
+exit(1);
+}
+
+printf("The result = %f\n",s[top]);
+}
